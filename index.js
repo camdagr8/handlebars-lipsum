cfaaafb7c351687e69b95787748ee0d73452ef3e
Cameron Tullos <ctullos@resourceammirati.com>
2017-02-06 13:25:01 -0500

    Updated to ensure the first and last characters aren’t weird.

--- index.js
+++ index.js
@@ -12,18 +12,55 @@ module.exports = function(from, to) {
 	output = output.replace(/\s{2,}/g," ");
 
 	if (from === 'random') {
-		var len = output.length - to;
+		let len = output.length - to;
 		from = Math.floor(Math.random() * len);
 	}
 
-
 	output = output.substr(from, to);
 	output = output.trim();
+	output = output.toLowerCase();
 
-	var first = output.substr(0, 1);
+	// Make sure first word isn't a random letter.
+	let words = output.split(' ');
+
+	let firstWord = words[0];
+	while (firstWord.length < 2) {
+
+		let l = firstWord.substr(0, 1);
+		if (l !== 'i' && l !== 'a') {
+			output = output.substr(2);
+			output = output.trim();
+
+			words = output.split(' ');
+			firstWord = words[0];
+		} else {
+			break;
+		}
+
+	}
+
+	// Make sure last word isn't a random letter.
+	let lastWord = words[words.length - 1];
+	while (lastWord.length < 3) {
+
+		let l = lastWord.substr(0, 1);
+		if (l !== 'i') {
+			let len = output.length - lastWord.length;
+			output = output.substr(0, len);
+			output = output.trim();
+
+			words = output.split(' ');
+			lastWord = words[words.length - 1];
+		} else {
+			break;
+		}
+	}
+
+	// Uppercase first letter
+	let first = output.substr(0, 1);
 	first = first.toUpperCase();
 
-	var rest = output.substring(1);
+	let rest = output.substring(1);
 
 	output = first + rest;
 
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var should = require('chai').should(),
     lipsum = require('../index.js');
 
+
 describe('#lipsum(0, 25)', function() {
   it('Chars 0-25', function() {
 	lipsum(0, 25).should.equal('Cras pretium scelerisque');
@@ -10,7 +11,7 @@ describe('#lipsum(0, 25)', function() {
 
 describe('#lipsum(25, 50)', function() {
   it('Chars 25-50', function() {
-	lipsum(25, 50).should.equal('Odio eget dignissim velit auctor at Nullam efficit');
+	lipsum(25, 50).should.equal('Odio eget dignissim velit auctor at nullam efficit');
   });
 });
 
